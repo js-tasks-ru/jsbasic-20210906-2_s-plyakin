b11e5a781d2af082b7102bc0e764ae7d05d9f88b
Sergei <s.plyakin@yandex.ru>
2021-09-08 10:36:56 +0200

    задача 1_4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,7 @@
 function checkSpam(str) {
-  // ваш код...
+    let a = '1xBet'.toLowerCase();
+    let b = 'XXX'.toLowerCase();
+    let str2 = str.toLowerCase();
+    if (str2.includes(a) || str2.includes(b)) return true;
+    return false;
 }
