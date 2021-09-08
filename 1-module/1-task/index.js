f07c232a33b8817374f173dccab27f3cefd8091c
Sergei <s.plyakin@yandex.ru>
2021-09-08 09:59:44 +0200

    задачи 1_1 и 1_2

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,9 @@
 function factorial(n) {
-  // ваш код...
+    // ваш код...
+    let data = 1;
+    if (n == 0 || n == 1) return 1;
+    for (let i = n; i > 0; i--) {
+        data *= i;
+    }
+    return data;
 }
