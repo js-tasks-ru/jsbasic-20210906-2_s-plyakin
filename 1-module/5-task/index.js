212eb33ee5d21b81890f6dc8095910dc524b0a0c
Sergei <s.plyakin@yandex.ru>
2021-09-08 10:47:08 +0200

    задача 1_5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,5 @@
 function truncate(str, maxlength) {
-  // ваш код...
+    // ваш код...
+    if (str.length == maxlength || str.length < maxlength) return str;
+    return `${str.slice(0, maxlength - 1)}…`;
 }
